d922806d7176e0493d018031f5eb3c952807019a
Herkermer Sherwood <theherk@gmail.com>
2015-11-09 12:50:09 -0800

    Return requests 404 when receive empty data set

--- pinkopy/commvault.py
+++ pinkopy/commvault.py
@@ -1,4 +1,5 @@
 from base64 import b64encode
+from collections import namedtuple
 from datetime import datetime, timedelta
 import logging
 import requests
@@ -42,8 +43,7 @@ class CommvaultSession(object):
         self.client_jobs = {}
         self.client_properties = {}
         self.subclients = {}
-        self.subclient_jobs = {'id': {}, 'name': {}}
-        self.job_details = None
+        self.subclient_jobs = {}
         self.job_vmstatus = None
         self.get_token()
         self.get_clients()
@@ -84,8 +84,12 @@ class CommvaultSession(object):
                 res.raise_for_status()
             else:
                 return res
-        except requests.exceptions.RequestException as e:
-            log.error(e)
+        except requests.exceptions.HTTPError as e:
+            raise
+        except Exception as e:
+            msg = 'Pinkopy request failed.'
+            log.exception(msg)
+            raise PinkopyError(msg)
 
     def get_token(self):
         """Login to Commvault and get token."""
@@ -104,8 +108,8 @@ class CommvaultSession(object):
             self.headers['Authtoken'] = data['DM2ContentIndexing_CheckCredentialResp']['@token']
             return self.headers['Authtoken']
         else:
-            log.error('Commvault user or pass incorrect')
-            raise ValueError('Commvault user or pass incorrect')
+            msg = 'Commvault user or pass incorrect'
+            raise_requests_error(401, msg)
 
     def get_clients(self):
         """Get list of clients from Commvault."""
@@ -114,27 +118,44 @@ class CommvaultSession(object):
             path = 'Client'
             res = self.request('GET', path)
             data = res.json()
-            self.clients_last_updated = datetime.now()
             return data['App_GetClientPropertiesResponse']['clientProperties']
 
         # Update this list at least once per hour.
         if (not self.clients
             or datetime.now() > self.clients_last_updated + timedelta(hours=1)):
-            self.clients = get_from_source(**locals())
+            try:
+                self.clients = get_from_source(**locals())
+                if not self.clients:
+                    msg = 'No clients found in Commvault'
+                    raise_requests_error(404, msg)
+                self.clients_last_updated = datetime.now()
+            except Exception as e:
+                msg = 'Could not retrieve clients from Commvault.'
+                log.exception(msg)
+                raise PinkopyError(msg)
         return self.clients
 
     def get_client(self, client_id):
         """Get info for one client from clients."""
+        client_id = str(client_id)
         if not self.clients:
             self.get_clients()
-        return list(filter(lambda x: x['clientId'] == client_id, self.clients))[0]
+        try:
+            return list(filter(
+                lambda x: x['client']['clientEntity']['@clientId'] == client_id,
+                self.clients
+            ))[0]
+        except IndexError as e:
+            msg = 'Client {} not in client list.'.format(client_id)
+            raise_requests_error(404, msg)
 
     def get_client_properties(self, client_id):
-        """Get list of clients from Commvault.
+        """Get client properties.
  
         This call replies in XML, because who cares about Accept headers right.
         So, we must take the reply in XML and convert it to JSON to maintain sanity.
         """
+        client_id = str(client_id)
         def get_from_source(**kwargs):
             log.info('Getting client properties from source')
             path = 'Client/{}'.format(client_id)
@@ -146,19 +167,32 @@ class CommvaultSession(object):
                 data = xmltodict.parse(res.text)
             else:
                 data = res.json()
-            self.client_properties[client_id] = {}
-            self.client_properties[client_id]['properties'] = data['App_GetClientPropertiesResponse']['clientProperties']
+            return data['App_GetClientPropertiesResponse']['clientProperties']
 
-        if client_id not in self.client_properties:
-            get_from_source(**locals())
+        if (client_id not in self.client_properties
+            or datetime.now() > self.client_properties[client_id]['last_updated'] + timedelta(hours=1)):
+            try:
+                self.client_properties[client_id] = {
+                    'properties': get_from_source(**locals()),
+                    'last_updated': datetime.now()
+                }
+            except Exception as e:
+                msg = 'Unable to get client properties for client {}'.format(client_id)
+                log.exception(msg)
+                raise PinkopyError(msg)
+            # If receive an empty data set, send 404.
+            if not self.client_properties[client_id]['properties']:
+                self.client_properties.pop(client_id, None)
+                msg = 'No client properties found for client {}'.format(client_id)
+                raise_requests_error(404, msg)
         else:
             # We already have a good dataset. Return it.
-            log.info('Using cached client jobs')
-
+            log.info('Using cached client properties')
         return self.client_properties[client_id]['properties']
 
     def get_subclients(self, client_id):
         """Get list of subclients for given client."""
+        client_id = str(client_id)
         def get_from_source(**kwargs):
             log.info('Getting subclients list from source for client {}'
                      .format(client_id))
@@ -172,13 +206,26 @@ class CommvaultSession(object):
 
         if (client_id not in self.subclients
             or datetime.now() > self.subclients[client_id]['last_updated'] + timedelta(hours=1)):
-            self.subclients[client_id] = {}
-            self.subclients[client_id]['last_updated'] = datetime.now()
-            self.subclients[client_id]['subclients'] = get_from_source(**locals())
-        return self.subclients[client_id]['subclients']
+            try:
+                self.subclients[client_id] = {
+                    'subclients': get_from_source(**locals()),
+                    'last_updated': datetime.now()
+                }
+            except Exception as e:
+                msg = 'Unable to get subclients for client {}'.format(client_id)
+                raise_requests_error(404, msg)
+            if not self.subclients[client_id]['subclients']:
+                self.subclients.pop(clients_id, None)
+                msg = 'No subclients for client {}'.format(client_id)
+                raise_requests_error(404, msg)
+        else:
+            # We already have a good dataset. Return it.
+            log.info('Using cached subclients')
+            return self.subclients[client_id]['subclients']
 
     def get_jobs(self, client_id, job_filter=None, last=None):
         """Get list of jobs for a given client and filter."""
+        client_id = str(client_id)
         def get_from_source(**kwargs):
             log.info('Getting client jobs from source')
             path = 'Job'
@@ -208,9 +255,22 @@ class CommvaultSession(object):
             log.info('Using cached client jobs')
         return self.client_jobs[client_id]['jobs']
 
-    def get_subclient_jobs(self, jobs, subclient_id, last=None):
+    def get_subclient_jobs(self, jobs, subclient_id=None,
+                           subclient_name=None, last=None):
         """Get list of jobs relevant to a specific subclient."""
-        def get_from_source(**kwargs):
+        subclient_id = str(subclient_id) if subclient_id else None
+        if (subclient_id is None and subclient_name is None):
+            msg = 'Cannot get subclient jobs without name or id'
+            log.error(msg)
+            raise PinkopyError(msg)
+        elif (subclient_id is not None and subclient_name is not None):
+            msg = ('Cannot get subclient jobs by both name and id. '
+                   'Selecting id by default.')
+            log.info(msg)
+
+        Subclient = namedtuple('Subclient', ['id', 'name'])
+
+        def get_from_source_by_id(**kwargs):
             """Retrieve new data.
 
             Since we do not yet have a matching dataset, we must go to
@@ -218,9 +278,7 @@ class CommvaultSession(object):
             """
             log.info('Getting subclient jobs from source using id {}'
                      .format(subclient_id))
-            self.subclient_jobs['id'][subclient_id] = {}
-            self.subclient_jobs['id'][subclient_id]['last'] = last
-            self.subclient_jobs['id'][subclient_id]['jobs'] = sorted(
+            return sorted(
                 [
                     job for job in jobs
                     if job['jobSummary']['subclient']['@subclientId'] == subclient_id
@@ -229,23 +287,7 @@ class CommvaultSession(object):
                 reverse=True
             )[:last]
 
-        if subclient_id not in self.subclient_jobs['id']:
-            get_from_source(**locals())
-        elif self.subclient_jobs['id'][subclient_id]['last'] != last:
-            get_from_source(**locals())
-        else:
-            # We already have a good dataset. Return it.
-            log.info('Using cached subclient jobs')
-        return self.subclient_jobs['id'][subclient_id]['jobs']
-
-    def get_subclient_jobs_by_name(self, jobs, subclient_name, last=None):
-        """Get list of jobs relevant to a specific subclient.
-
-        Given a list of jobs and subclient name match subclient jobs.
-        This makes the significant assumption that the subclient name
-        will be contained in @subclientName.
-        """
-        def get_from_source(**kwargs):
+        def get_from_source_by_name(**kwargs):
             """Retrieve new data.
 
             Since we do not yet have a matching dataset, we must go to
@@ -253,9 +295,7 @@ class CommvaultSession(object):
             """
             log.info('Getting subclient jobs from source using name {}'
                      .format(subclient_name))
-            self.subclient_jobs['name'][subclient_name] = {}
-            self.subclient_jobs['name'][subclient_name]['last'] = last
-            self.subclient_jobs['name'][subclient_name]['jobs'] = sorted(
+            return sorted(
                 [
                     job for job in jobs
                     if subclient_name in job['jobSummary']['subclient']['@subclientName']
@@ -264,17 +304,45 @@ class CommvaultSession(object):
                 reverse=True
             )[:last]
 
-        if subclient_name not in self.subclient_jobs['name']:
-            get_from_source(**locals())
-        elif self.subclient_jobs['name'][subclient_name]['last'] != last:
-            get_from_source(**locals())
+        if subclient_id:
+            try:
+                subclient = [_ for _ in self.subclient_jobs if _.id == subclient_id][0]
+            except IndexError as e:
+                # subclient not yet cached
+                subclient = None
+        elif subclient_name:
+            try:
+                subclient = [_ for _ in self.subclient_jobs if _.name == subclient_name][0]
+            except IndexError as e:
+                # subclient not yet cached
+                subclient = None
+        if (not subclient
+            or subclient['last'] != last
+            or datetime.now() > subclient['last_updated'] + timedelta(hours=1)):
+            _subclient_jobs = {
+                'jobs': get_from_source_by_id(**locals()) if subclient_id else get_from_source_by_name(**locals()),
+                'last': last,
+                'last_updated': datetime.now()
+            }
+            try:
+                subclient = Subclient(
+                    id=_subclient_jobs['jobs'][0]['jobSummary']['subclient']['@subclientId'],
+                    name=_subclient_jobs['jobs'][0]['jobSummary']['subclient']['@subclientName']
+                )
+                self.subclient_jobs[subclient] = _subclient_jobs
+            except IndexError as e:
+                msg = ('No subclient jobs found for subclient_id {} / subclient_name {}'
+                       .format(subclient_id, subclient_name))
+                raise_requests_error(404, msg)
         else:
             # We already have a good dataset. Return it.
             log.info('Using cached subclient jobs')
-        return self.subclient_jobs['name'][subclient_name]['jobs']
+        return self.subclient_jobs[subclient]['jobs']
 
     def get_job_details(self, client_id, job_id):
         """Get details about a given job."""
+        client_id = str(client_id)
+        job_id = str(job_id)
         path = 'JobDetails'
         payload = {
             'JobManager_JobDetailRequest': {
@@ -284,8 +352,15 @@ class CommvaultSession(object):
         #<JobManager_JobDetailRequest jobId="2575"/>
         res = self.request('POST', path, payload=payload)
         data = res.json()
-        self.job_details = data['JobManager_JobDetailResponse']['job']['jobDetail']
-        return self.job_details
+        try:
+            job_details = data['JobManager_JobDetailResponse']['job']['jobDetail']
+        except TypeError as e:
+            msg = 'No job details found for job {}'.format(job_id)
+            raise_requests_error(404, msg)
+        if not job_details:
+            msg = 'No job details found for job {}'.format(job_id)
+            raise_requests_error(404, msg)
+        return job_details
 
     def get_job_vmstatus(self, job_details):
         """Get all vmStatus entries for a given job."""
@@ -298,9 +373,9 @@ class CommvaultSession(object):
             if isinstance(vms, dict):
                 # Only one vmStatus
                 vms = [vms]
-            else:
-                # Already a list, populate it
-                vms = [s for s in vms]
+        else:
+            msg = 'No vmstatus in job details'
+            raise_requests_error(404, msg)
         self.job_vmstatus = vms
         return self.job_vmstatus
 
