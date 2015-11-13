edfc0ae427eb5e43df1f921b2ba543e1e15488f3
Herkermer Sherwood <theherk@gmail.com>
2015-11-13 15:18:22 -0800

    Improve relogin attempts
    Fixes #9

--- pinkopy/commvault.py
+++ pinkopy/commvault.py
@@ -55,8 +55,15 @@ class CommvaultSession(object):
         self.logout()
 
     def request(self, method, path, qstr_vals=None, service=None,
-                headers=None, payload=None, **kwargs):
+                headers=None, payload=None, attempt=None, **kwargs):
         """Make a request to Commvault."""
+        # We may need to recall the same request.
+        # Must pop self because it is passed implicitly and cannot
+        # be passed twice.
+        _context = {k: v for k, v in locals().items() if k is not 'self'}
+        allowed_attempts = 3
+        attempt = 1 if not attempt else attempt
+
         service = service if service else self.service
         headers = headers if headers else self.headers
         try:
@@ -68,18 +75,24 @@ class CommvaultSession(object):
                 res = requests.get(service + path, headers=headers, params=payload)
             else:
                 raise ValueError('HTTP method {} not supported'.format(method))
-            if res.status_code == 401 and headers['Authtoken'] is not None:
-                # token went bad, login again
+            if (res.status_code == 401
+                and headers['Authtoken'] is not None
+                and attempt <= allowed_attempts):
+                # Token went bad, login again.
                 log.info('Commvault token logged out. Logging back in.')
-                # delay is so I don't get into recursion trouble if I can't login right away
+                # Delay is so I don't get into recursion trouble if I can't login right away.
                 time.sleep(5)
                 self.get_token()
-                # We need to recall with the same request to continue.
-                # Must pop self because it is passed implicitly and cannot
-                # be passed twice.
-                _ = locals()
-                del _['self']
-                return self.request(**_)
+                # Recall the same function, after having logged back into Commvault.
+                attempt += 1
+                _context['attempt'] = attempt
+                return self.request(**_context)
+            elif attempt > allowed_attempts:
+                # Commvault probably down, raise exception.
+                msg = ('Could not log back into Commvault after {} '
+                       'attempts. It could be down.'
+                       .format(allowed_attempts))
+                raise_requests_error(401, msg)
             elif res.status_code != 200:
                 res.raise_for_status()
             else:
