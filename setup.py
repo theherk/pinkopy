1afa3c51897f85befcb0e14cda06fb75729f98b9
Herkermer Sherwood <theherk@gmail.com>
2015-11-19 15:23:34 -0800

    Update setup.py to develop version

--- setup.py
+++ setup.py
@@ -14,13 +14,13 @@ install_requires = [
 
 setup(
     name='pinkopy',
-    version='1.2.0',
+    version='1.3.dev',
     description='Python wrapper for Commvault api',
     long_description=readme,
     author='Herkermer Sherwood',
     author_email='theherk@gmail.com',
     url='https://github.com/theherk/pinkopy',
-    download_url='https://github.com/theherk/pinkopy/archive/1.2.0.zip',
+    download_url='https://github.com/theherk/pinkopy/archive/1.3.dev.zip',
     packages=find_packages(),
     platforms=['all'],
     license='MIT',
